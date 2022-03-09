c16f945f2d7273d15018add47c16198fdf56fbd2
jason <jduong89@gmail.com>
2022-03-09 09:54:30 -0800

    adds route for all products and path

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "toy car", price: 10, description: "a plastic red car")
+product.save
+product = Product.new(name: "watch", price: 20, description: "a plastic gold watch with a printed on watch face")
+product.save
