656cda67814c07b46b729d556018937fde18b803
jason <jduong89@gmail.com>
2022-03-09 10:42:44 -0800

    adds more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
-product = Product.new(name: "toy car", price: 10, description: "a plastic red car")
+# product = Product.new(name: "toy car", price: 10, description: "a plastic red car")
+# product.save
+# product = Product.new(name: "watch", price: 20, description: "a plastic gold watch with a printed on watch face")
+# product.save
+product = Product.new(name: "teddy bear", price: 12, description: "stuffed toy bear")
 product.save
-product = Product.new(name: "watch", price: 20, description: "a plastic gold watch with a printed on watch face")
+product = Product.new(name: "apple", price: 2, description: "real fuji apple")
+product.save
+product = Product.new(name: "speakers", price: 50, description: "black pair of speakers")
+product.save
+product = Product.new(name: "lamp", price: 29, description: "a plastic white lamp. no bulbs included")
 product.save
