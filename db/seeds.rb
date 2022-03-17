85717e61906fa37e5ea04371fc6ea788dd5f76c9
jason <jduong89@gmail.com>
2022-03-17 10:54:00 -0700

    adds supplier id attribute to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,13 @@
 # product.save
 # product = Product.new(name: "watch", price: 20, description: "a plastic gold watch with a printed on watch face")
 # product.save
-product = Product.new(name: "teddy bear", price: 12, description: "stuffed toy bear")
-product.save
-product = Product.new(name: "apple", price: 2, description: "real fuji apple")
-product.save
-product = Product.new(name: "speakers", price: 50, description: "black pair of speakers")
-product.save
-product = Product.new(name: "lamp", price: 29, description: "a plastic white lamp. no bulbs included")
-product.save
+# product = Product.new(name: "teddy bear", price: 12, description: "stuffed toy bear")
+# product.save
+# product = Product.new(name: "apple", price: 2, description: "real fuji apple")
+# product.save
+# product = Product.new(name: "speakers", price: 50, description: "black pair of speakers")
+# product.save
+# product = Product.new(name: "lamp", price: 29, description: "a plastic white lamp. no bulbs included")
+# product.save
+supplier = Supplier.create(name: "amazon", email: "amazon@amazon.com", phone_number: "dont-call-me")
+supplier = Supplier.create(name: "apple", email: "apple@apple.com", phone_number: "call-amazon")
