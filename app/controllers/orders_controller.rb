de746f8f3f9036b9e82a323df07822ef99746f21
jason <jduong89@gmail.com>
2022-03-21 12:27:14 -0700

    adds create&show actions/routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
+  def create
+    order = Order.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+    order.save
+    render json: order.as_json
+  end
+
+  def show
+    order = Order.find(params[:id])
+    render json: order.as_json
+  end
 end
