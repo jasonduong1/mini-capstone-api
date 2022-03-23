e481a9a5f51c20a60508efef68e9f295106488c6
jason <jduong89@gmail.com>
2022-03-22 18:04:39 -0700

    changes create so subtotal, total, and tax comes from calculations

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,19 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
+    product = Product.find(params[:product_id])
+
+    calculated_subtotal = product.price * params[:quantity]
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
     order.save
     render json: order.as_json
