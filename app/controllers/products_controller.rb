62edbacca2b4024eb7d38a98bd3a76842f0b5f6a
jason <jduong89@gmail.com>
2022-03-24 17:06:14 -0700

    adds if condition to index for categor params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,11 @@ class ProductsController < ApplicationController
   before_action :authenticate_admin, except: [:index, :show]
 
   def index
-    pp current_user
     @products = Product.all
+    if params[:category]
+      category = Category.find_by(name: params[:category])
+      @products = category.products
+    end
     render :index
   end
 
