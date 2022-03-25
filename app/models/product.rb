650db889968f4d36f7e82ab99dcc3eafe3eb35f5
jason <jduong89@gmail.com>
2022-03-25 12:34:22 -0700

    adds associations with carted_products

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
 
+  has_many :carted_products
   belongs_to :supplier
   # def supplier
   #   Supplier.find_by(id: supplier_id)
