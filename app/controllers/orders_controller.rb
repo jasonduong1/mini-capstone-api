c2e240cedddc4ec683bad75dcdb6d44fc557ffd0
jason <jduong89@gmail.com>
2022-03-28 15:00:11 -0700

    moves total calculations to order model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,23 +2,12 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    carted_products = current_user.carted_products.where(status: "carted")
-
-    calculated_subtotal = 0
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-    end
-
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.new(
       user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
     )
     @order.save
+    carted_products = current_user.carted_products.where(status: "carted")
+    @order.update_totals
     carted_products.update_all(status: "purchased", order_id: @order.id)
     render :show
   end
